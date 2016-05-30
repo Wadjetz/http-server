4dc96e3eca732b3701908bb44a5149b345aba1f4
Wadjetz <egor.neon@gmail.com>
2016-05-30 17:35:38 +0200

    first commit

---
+++ src/main/java/fr/wadjetz/http/server/Main.java
@@ -0,0 +1,7 @@
+package fr.wadjetz.http.server;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/fr/wadjetz/http/server/HelloTest.java
@@ -0,0 +1,14 @@
+package fr.wadjetz.http.server;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void testMessage() {
+        assertTrue(true);
+    }
+
+}
